64efabd5be0de65cccbabf076def9a49dd235eea
Vegz78 <49032025+Vegz78@users.noreply.github.com>
2024-10-08 17:49:27 +0200

    Add files via upload
    Small fix

--- pqdiag.bat
+++ pqdiag.bat
@@ -33,7 +33,7 @@ SET /A CONNECT_TIMEOUT=5
     setlocal enableextensions enabledelayedexpansion
     call :validateIP %1 ret
     if !ret! EQU 0 (
-        echo Sending print quality diagnostics job to HP_printer@%1!
+        echo Sending print quality diagnostics job to HP_printer@%1^^!
         curl --max-time !MAX_TIME! --connect-timeout !CONNECT_TIMEOUT! -H "User-Agent:" -H "Host: localhost" -H "Accept:" -H "X-HP-AR-INFO: chunk-correct" -H "Content-Type: text/xml" -H "Content-Length: 220" --data-binary "<?xml version=\"1.0\" encoding=\"UTF-8\"?><ipdyn:InternalPrintDyn xmlns:ipdyn=\"http://www.hp.com/schemas/imaging/con/ledm/internalprintdyn/2008/03/21\"><ipdyn:JobType>pqDiagnosticsPage</ipdyn:JobType></ipdyn:InternalPrintDyn>" --http1.1 http://%1%:8080/DevMgmt/InternalPrintDyn.xml >nul 2>&1
     if not errorlevel 1 (
         echo PQ diagnostics printed OK^^!
