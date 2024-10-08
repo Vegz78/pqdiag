8543813050a65d884b7cef911738ab6eae9b714b
Vegz78 <49032025+Vegz78@users.noreply.github.com>
2024-10-08 17:45:53 +0200

    Add files via upload
    Small bug fixes and license information

--- pqdiag.bat
+++ pqdiag.bat
@@ -1,4 +1,32 @@
 @ECHO OFF
+rem Scripts for scheduling print quality diagnostics reports (PQ Diagnostics) 
+rem on HP printers, for Windows, MacOS and Linux.
+rem 
+rem This file is a part of the https://github.com/vegz78/pqdiag repository.
+rem
+rem Feel free to use and develop, but don't forget to inlcude proper attribution
+rem to the developer.
+rem
+rem MIT License * * Copyright (c) 2024 Vegz78
+rem
+rem Permission is hereby granted, free of charge, to any person obtaining a copy
+rem of this software and associated documentation files (the "Software"), to deal
+rem in the Software without restriction, including without limitation the rights
+rem to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+rem copies of the Software, and to permit persons to whom the Software is
+rem furnished to do so, subject to the following conditions:
+rem
+rem The above copyright notice and this permission notice shall be included in all
+rem copies or substantial portions of the Software.
+rem
+rem THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+rem IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+rem FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+rem AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+rem LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+rem OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+rem SOFTWARE.
+
 SET /A MAX_TIME=5
 SET /A CONNECT_TIMEOUT=5
 
@@ -8,14 +36,14 @@ SET /A CONNECT_TIMEOUT=5
         echo Sending print quality diagnostics job to HP_printer@%1!
         curl --max-time !MAX_TIME! --connect-timeout !CONNECT_TIMEOUT! -H "User-Agent:" -H "Host: localhost" -H "Accept:" -H "X-HP-AR-INFO: chunk-correct" -H "Content-Type: text/xml" -H "Content-Length: 220" --data-binary "<?xml version=\"1.0\" encoding=\"UTF-8\"?><ipdyn:InternalPrintDyn xmlns:ipdyn=\"http://www.hp.com/schemas/imaging/con/ledm/internalprintdyn/2008/03/21\"><ipdyn:JobType>pqDiagnosticsPage</ipdyn:JobType></ipdyn:InternalPrintDyn>" --http1.1 http://%1%:8080/DevMgmt/InternalPrintDyn.xml >nul 2>&1
     if not errorlevel 1 (
-        echo "PQ diagnostics printed OK!"
+        echo PQ diagnostics printed OK^^!
     ) else (
-        echo "PQ diagnostics did NOT print!"
+        echo PQ diagnostics did NOT print^^!
     )
 	) else (
-        echo "The argument \"%1\" is not a valid IPv4 address or missing..."
-	    echo "Please try again like this: pqdiag.bat 192.0.0.10"
-	    echo "(Using your HP printer's IP address)"
+        echo The argument \"%1\" is not a valid IPv4 address or missing...
+	    echo Please try again like this: pqdiag.bat 192.0.0.10
+	    echo (Using your HP printer's IP address^)
     )
     exit /b 
 
