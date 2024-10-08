08696d8012c477bc664d9caf0eb0cd386b126717
Vegz78 <49032025+Vegz78@users.noreply.github.com>
2024-10-08 17:50:13 +0200

    Add files via upload
    Small bug fixes and license information

--- pqdiag.sh
+++ pqdiag.sh
@@ -1,4 +1,33 @@
 #!/bin/bash
+
+# Scripts for scheduling print quality diagnostics reports (PQ Diagnostics) 
+# on HP printers, for Windows, MacOS and Linux.
+#
+# This file is a part of the https://github.com/vegz78/pqdiag repository.
+#
+# Feel free to use and develop, but don't forget to inlcude proper attribution
+# to the developer.
+#
+# MIT License * * Copyright (c) 2024 Vegz78
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 MAX_TIME=5
 CONNECT_TIMEOUT=5
 
